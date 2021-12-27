0cee152e2615c7c08c03327a0d76bb3a5012ab80
daniel-munro <dan@dmun.ro>
2021-12-27 14:06:50 -0600

    Add stats monitoring

---
+++ api/stats.sh
@@ -0,0 +1,16 @@
+DB=/Genomics/demask/data/demask.db
+STATS=/var/www/demask/api/stats.txt
+STATS_DB=/var/www/demask/api/stats_db.txt
+
+# Start entry with the date
+date >> $STATS
+
+# Show disk usage for / (input, working, and output data) and /var (site and blast db) partitions
+df -h | grep -P '/(var)?$' >> $STATS
+
+# Total jobs in database
+sqlite3 $DB "SELECT COUNT(sha1) FROM Scoreset" | awk '{print $1" proteins in database"}' >> $STATS
+
+# Dump entire job database into a text file
+sqlite3 $DB "PRAGMA table_info(Scoreset)" | cut -d'|' -f2 | tr '\n' '\t' | sed 's/\t$/\n/' > $STATS_DB
+sqlite3 $DB "SELECT * FROM Scoreset" | sed 's/|/\t/g' >> $STATS_DB
